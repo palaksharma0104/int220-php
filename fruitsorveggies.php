6d908273d5384fb26b52ccb7588ae4f748449874
palaksharma0104 <palak16221@gmail.com>
2024-09-18 09:49:00 +0530

    commit

---
+++ fruitsorveggies.php
@@ -0,0 +1,20 @@
+<?php
+
+$fruits = array(
+    "apple" => 10,
+    "banana" => 5,
+    "orange" => 8
+);
+
+$vegetables = array(
+    "carrot" => 12,
+    "broccoli" => 6,
+    "spinach" => 9
+);
+
+$fruit_count = count($fruits);
+$vegetable_count = count($vegetables);
+
+echo "Number of fruits: " . $fruit_count . "\n";
+echo "Number of vegetables: " . $vegetable_count . "\n";
+?>
