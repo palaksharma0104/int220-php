7c10e3fd2058f27b8b30eca9925d5cb5a5cb0b43
palaksharma0104 <palak16221@gmail.com>
2024-10-11 16:24:32 +0530

    commit

--- form/form.php
+++ form/form.php
@@ -8,16 +8,7 @@ $age = $_POST['age'];
 
 if (empty($name) || empty($email) || empty($message) || empty($age)) {
     echo "Please fill out all fields.";
-} else {
- 
-    $to = "your_email@example.com";
-    $subject = "Contact Form Submission";
-    $body = "Name: " . $name . "\nEmail: " . $email . "\nMessage: " . $message;
-
-    if (mail($to, $subject, $body)) {
-        echo "Your message has been sent successfully.";
-    } else {
-        echo "There was an error sending your message.";
-    }
+    
 }
+
 ?>
